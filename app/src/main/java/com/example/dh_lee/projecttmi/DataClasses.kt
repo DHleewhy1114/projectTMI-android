6841f515a9128e3250d7d3ea6be101ff0bc0f815
dh_leewhy <dlehd14@gmail.com>
2018-10-17 13:54:07 +0900

    first setting

---
+++ app/src/main/java/com/example/dh_lee/projecttmi/DataClasses.kt
@@ -0,0 +1,18 @@
+package com.example.dh_lee.projecttmi
+
+//http통신시 데이터 파싱을 위한 인터페이스
+
+interface HTTPResponseData{
+    val id:String//각 통신시 기본적으로 필요한 id
+
+}
+
+
+data class TimelineResponseData(
+        override val id:String, val author:String,val contents:String,val edited:String):HTTPResponseData
+
+data class MatchingResponseData(
+        override val id:String, val user_id:String):HTTPResponseData
+
+
+
